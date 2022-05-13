dc18f383cd19dcedbb641dd1263bfb9c42bc561b
gabrielscholze-r <oliverfantasma24@gmail.com>
2022-05-13 01:46:04 -0300

    homepage styles

--- frontend/src/components/furniture/index.js
+++ frontend/src/components/furniture/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './index.css'
+import excludeSignal from '../../assets/img/exclude.png'
+
 export default function Furniture({data}){
     console.log(data)
     return (
-        <div class="content-container py-2">
+        <div className="body-furniture">
+        <img src={excludeSignal} alt="exclude_signal" className="exclude-signal mr-1"/>
+        <div class="content-container py-3 my-1">
             <h3 class="subtitle-furniture px-2">Nome:</h3>
             <h4 className="info-furniture">{data.name}</h4>
             <br/>
@@ -11,7 +15,10 @@ export default function Furniture({data}){
             <h4 className="info-furniture">{data.price}</h4>
             <br/>
             <h3 class="subtitle-furniture px-2">Amount:</h3>
-            <h4 className="info-furniture">{data.qtd}</h4>
+            <h4 className="info-furniture pr-3">{data.qtd}</h4>
+            
+        </div>
+        
         </div>
     )
 }
--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -15,13 +15,13 @@ function Homepage() {
     }, [furniture])
     
     return (
-        <div>
+        <div className="homepage-container">
             <header className="header">
 
                 <div className="px-5"><img src={logo} alt="logo" className="header-logo" /></div>
                 <div className="px-5"><button className="exitbutton" role="button">Sair</button></div>
             </header>
-            <div className='filter-search'>
+            <div className='filter-search mt-5'>
                 <select className="select-input" name="furniture" value={furniture} onChange={e => setFurniture(e.target.value)}>
                     <option value="sofa" >Sofa</option>
                     <option value="chair" >Chair</option>
